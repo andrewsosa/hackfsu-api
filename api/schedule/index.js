7d03d414aa8f7910fb6470a9fb1c74982d2b95b2
Andrew Sosa <andrew@andrewsosa.com>
2019-01-21 17:41:14 -0500

    Add dev server, add schedule comments

--- api/schedule/index.js
+++ api/schedule/index.js
@@ -1,3 +1,4 @@
+const { send } = require("micro");
 const base = require("airtable").base("appc0VYcl3q4dlHmg");
 
 const columns = [
@@ -11,29 +12,37 @@ const columns = [
   "End Time"
 ];
 
+// Simple func for extracting our column list and mapping it to a
+// JSON object.
 const parse = (record, fields) =>
   fields.reduce((map, field) => {
     map[field] = record.get(field);
     return map;
   }, {});
 
-module.exports = (req, res) => {
-  res.setHeader("Content-Type", "application/json");
-  const payload = [];
-  // Read from table
-  base("Schedule")
-    .select()
-    .eachPage(
-      (records, next) => {
-        records.forEach(rec => {
-          payload.push(parse(rec, columns));
-          console.log(rec);
-        });
-        next();
-      },
-      err => {
-        if (err) console.error(err);
-        else res.end(JSON.stringify(payload));
-      }
-    );
+// Load the schedule using an async promise. By using a promise,
+// we force the response to wait for our result.
+const schedule = async () => {
+  return new Promise(resolve => {
+    const payload = [];
+    // Read from table
+    base("Schedule")
+      .select()
+      .eachPage(
+        (records, next) => {
+          records.forEach(rec => {
+            payload.push(parse(rec, columns));
+          });
+          next();
+        },
+        err => {
+          if (err) console.error(err);
+          resolve(payload);
+        }
+      );
+  });
+};
+
+module.exports = async (req, res) => {
+  send(res, 200, await schedule());
 };
